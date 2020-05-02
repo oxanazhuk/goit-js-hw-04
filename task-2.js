7b99e03b2329d84ef1b49a52af1ac304b966d463
Zhuk Oxana <58565993+oxanazhuk@users.noreply.github.com>
2020-05-02 17:36:58 +0300

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,31 @@
+'use strict';
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+  action.call(inventory, itemName);
+};
+
+
+
+invokeInventoryAction('Аптечка', inventory.add);
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+
+invokeInventoryAction('Фильтр', inventory.remove);
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
